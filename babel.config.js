8e7144be206bb37126cc4ac39ecbdb185e8dd02d
Rickard Natt och Dag <rickard@hey.com>
2021-03-04 12:34:52 +0100

    chore(ci): add babel class properties

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  plugins: ['@babel/plugin-proposal-class-properties'],
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
